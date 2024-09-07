aa9e1d330bc0d919c8a279df963c3700ca678128
Evangelos Meklis <57478341+evangelosmeklis@users.noreply.github.com>
2024-09-07 14:28:57 +0300

    new release fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="peepdb",
-    version="0.1.1",
+    version="0.1.2",
     author="Evangelos Meklis",
     author_email="vmeklis@hotmail.com",
     description="A quick database table viewer",
